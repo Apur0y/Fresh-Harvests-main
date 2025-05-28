47298fc1add22227462663bee1973bb190f8adde
Apur0y <apuroy2785@gmail.com>
2025-05-29 01:50:56 +0600

    added the products

--- sections/Products.tsx
+++ sections/Products.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+
+  type Product =  {
+        name:string,
+        weight:string,
+        image:string,
+        category: 'fruit' | 'vegetable' | 'salad'
+    }
 
 export default function Products() {
 
+
+  
     const products=[
         {
             name:"Mushroom",
@@ -11,13 +21,26 @@ export default function Products() {
         }
     ]
 
+    const [isActive, setIsActive]=useState<string> ('all');
+    const [products, setProducts]=useState<Product[]>([]);
+    const 
+
+    
+
   return (
     <div>
 
-        <div className='flex justify-center items-center flex-col mt-44'>
+        <div className='header-align'>
         <p className='intro'>Our Products</p>
         <h1 className='heading'>Our Fresh Products</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus voluptas consequuntur ratione blanditiis sunt inventore rerum saepe soluta quos quae.</p>
+        <p className='para'>We Pride ourselves on offering a wide variety of fresh and flavourful fruits, <br /> vegetables, and salad ingredients</p>
+        </div>
+
+        <div>
+            <button>All</button>
+            <button>Fruits</button>
+            <button>Vegetables</button>
+            <button>Salad</button>
 
         </div>
         
