59095b0243edd1474f3a8d0ae3a8a927e3bd63de
Apur0y <apuroy2785@gmail.com>
2025-05-30 09:35:52 +0600

    check commit

--- app/(root)/loading.tsx
+++ app/(root)/loading.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function loading() {
   return (
-    <div>
+    <div className='min-h-screen flex justify-center items-center'>
    <Spinner></Spinner>
     </div>
   )
